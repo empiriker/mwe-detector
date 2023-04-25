52facb6f3fd6f1c8c4b01d0dc178d535f9291f1a
Empiriker <till.ueberfries@gmail.com>
2023-04-25 14:29:54 +0200

    add python package setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="mwe_detector",
+    version="0.0.1",
+    description="A SpaCy MWEDetector pipeline component",
+    url="https://github.com/empiriker/mwe_detection.git#egg=mwe_detector&subdirectory=mwe_detector",
+    author="Till ÜF",
+    author_email="till@ueberfries.de",
+    license="All rights reserved",
+    packages=["mwe_detector"],
+    zip_safe=False,
+)
