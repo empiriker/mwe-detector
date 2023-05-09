f7587062b7d9a290bbf81288a54cd1fc0bb596af
Empiriker <till.ueberfries@gmail.com>
2023-05-09 14:57:07 +0200

    expose model as spacy pipeline

---
+++ mwe_detector/pipeline.py
@@ -0,0 +1,17 @@
+from spacy.lang.fr import French
+from spacy.lang.en import English
+from mwe_detector.model import MWEDetector
+
+
+@French.factory("mwe_detector")
+def create_mwe_detector(nlp, name):
+    mweDetector = MWEDetector(nlp)
+    mweDetector.from_disk("data")
+    return mweDetector
+
+
+@English.factory("mwe_detector")
+def create_mwe_detector(nlp, name):
+    mweDetector = MWEDetector(nlp)
+    mweDetector.from_disk("data")
+    return mweDetector
