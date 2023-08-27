5e93c6a064fbfc47c1ca0f3f13aa9a69c1737aac
Empiriker <till.ueberfries@gmail.com>
2023-08-27 16:23:29 +0400

    add correct spacy factory decorators

--- mwe_detector/pipeline.py
+++ mwe_detector/pipeline.py
@@ -7,15 +7,27 @@ import os
 FN = os.path.join(os.path.dirname(__file__), "data")
 
 
-@French.factory("mwe_detector")
-def create_mwe_detector(nlp, name):
+assigns = ["token._.wikt_mwe"]
+requires = ["token.lemma", "token.pos", "token.dep", "token.head", "token.morph"]
+
+
+@French.factory(
+    "mwe_detector",
+    assigns=assigns,
+    requires=requires,
+)
+def create_mwe_detector_fr(nlp, name):
     mweDetector = MWEDetector(nlp)
     mweDetector.from_disk(FN)
     return mweDetector
 
 
-@English.factory("mwe_detector")
-def create_mwe_detector(nlp, name):
+@English.factory(
+    "mwe_detector",
+    assigns=assigns,
+    requires=requires,
+)
+def create_mwe_detector_en(nlp, name):
     mweDetector = MWEDetector(nlp)
     mweDetector.from_disk(FN)
     return mweDetector
